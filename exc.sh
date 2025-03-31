208d9424d2f1c78bd363cc373e211b134134efd8
Ubuntu <IETI@RenovAR.m3cqph3ogoku3fcfyxdjg3yxig.bx.internal.cloudapp.net>
2025-03-31 01:25:12 +0000

    shell

--- exc.sh
+++ exc.sh
@@ -4,6 +4,6 @@ sudo docker stop products
 git pull
 sudo docker rm products
 sudo docker build -t products .
-sudo docker run -d --name products products
+sudo docker run -d -p 8082:8082 --name products products
 
 
